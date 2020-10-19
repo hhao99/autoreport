47a7c930f562314de887ebd0281f89b121fac33a
Simon Dong <snovian@gmail.com>
2020-10-19 15:15:12 +0800

    test

--- src/config/globals.py
+++ src/config/globals.py
@@ -6,12 +6,14 @@ class GlobalConfig(object):
     TESTING = False
     CSRF_ENABLE = True
     DATA_PATH = './data.xlsx'
+    PLANNING_DATA='./planning.csv'
 
 
 class DevelopmentConfig(GlobalConfig):
     DEBUG = True
     DEVELOPMENT = True
     DATA_PATH = join(dirname(__file__), "../sample/data.xlsx")
+    PLANNING_DATA = join(dirname(__file__),"../sample/planning.csv")
 
 
 class ProductionConfig(GlobalConfig):
--- src/web/main.py
+++ src/web/main.py
@@ -1,12 +1,21 @@
 from flask import Flask, make_response, jsonify
 from config.globals import DevelopmentConfig
+import pandas as pd
+import traceback
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig())
 
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+@app.errorhandler(500)
+def server_error(error):
+    return make_response(jsonify({'error': 'Internal Server Error:' + traceback.print_stack()}), 500)
+
 
 @app.route('/')
 def index():
@@ -15,26 +24,18 @@ def index():
 
 @app.route('/get/<field>')
 def field_query(field):
-    import pandas as pd
-    from flask import jsonify
-
-    try:
-        df = pd.read_excel(app.config['DATA_PATH'])
-        return jsonify((list(set(df[field]))))
-    except:
-        return 'field exception'
+    df = pd.read_csv(app.config['PLANNING_DATA'])
+    df.columns = df.columns.map(lambda x: x.lower().strip().replace(' ', '_').replace('/', '_'))
+    values = df.groupby(field.lower()).size().reset_index()[field.lower()]
+    return jsonify(list(set(values)))
 
 
 @app.route('/queryby/<field>/<value>/<target_field>')
 def query_by(field, value, target_field):
-    import pandas as pd
-    from flask import jsonify
-    try:
-        df = pd.read_excel(app.config['DATA_PATH'])
-        df1 = df[df[field] == value]
-        return jsonify(list(set(df1[target_field])))
-    except:
-        return "exception"
+    df = pd.read_excel(app.config['DATA_PATH'])
+    df1 = df[df[field] == value]
+    return jsonify(list(set(df1[target_field])))
+
 
 
 # Press the green button in the gutter to run the script.
